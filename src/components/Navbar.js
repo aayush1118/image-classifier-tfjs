3ea1ea7e22b3d2772c1ce5f9882dafaad84620ed
Aayush jain <aayushjain1118@gmail.com>
2021-10-20 20:57:41 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { BiSearchAlt, BiCodeAlt, BiGridAlt } from 'react-icons/bi';
 
-function Navbar() {
+const Navbar = () => {
 	return (
 		<div className='fixed top-0 w-screen flex justify-center bg-gray-900 text-white py-1 shadow-lg'>
-			<i>A</i>
-			<i>B</i>
-			<i>C</i>
-			<i>D</i>
+			<Icon icon={<BiSearchAlt size='28' />} />
+			<Icon icon={<BiGridAlt size='28' />} />
+			<Icon icon={<BiCodeAlt size='28' />} />
 		</div>
 	);
-}
+};
+
+const Icon = ({ icon }) => <div className='navbar-icon group'>{icon}</div>;
 
 export default Navbar;
