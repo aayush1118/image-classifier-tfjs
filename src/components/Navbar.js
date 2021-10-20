06431db2e43080af39b6f51ed0924aa1c645c39e
Aayush jain <aayushjain1118@gmail.com>
2021-10-20 19:54:12 +0530

    init

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+	return (
+		<div className='fixed top-0 w-screen flex justify-center bg-gray-900 text-white py-1 shadow-lg'>
+			<i>A</i>
+			<i>B</i>
+			<i>C</i>
+			<i>D</i>
+		</div>
+	);
+}
+
+export default Navbar;
